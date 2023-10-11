fd32d4cb0da88804a758b3ed4af906eb69a93a14
Y2IN <dpdls0228@daum.net>
2023-10-11 19:13:53 +0900

    Feat: favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,14 +7,29 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-mdx", {
+  plugins: [
+    "gatsby-plugin-styled-components", 
+    "gatsby-plugin-mdx", 
+    {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
       "path": "./src/pages/"
     },
     __key: "pages"
-  }]
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `yeins portfolio`,
+      short_name: `yein's portfolio`,
+      start_url: `/`,
+      background_color: `#f7f0eb`,
+      theme_color: `#a2466c`,
+      display: `standalone`,
+      icon: `./src/images/icon.png`
+    },},
+  ]
 };
 
 export default config;
