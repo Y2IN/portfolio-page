e667a79e58575dc4c7aa8523681d6b0f1776b4a2
Y2IN <dpdls0228@daum.net>
2023-10-11 19:14:09 +0900

    Feat: mainBanner 추가

--- gatsby-site-meta-data.ts
+++ gatsby-site-meta-data.ts
@@ -5,6 +5,6 @@ export default {
       introduce1: "안녕하세요!",
       introduce2: "저는",
       introduce3: "를 좋아하는 개발자 제예인입니다.",
-      skills: ["frontend", "react", "typescript", "python", "iot", "aduino", "embedded"]
+      skills: ["frontend", "react", "typescript", "python", "c++", "iot", "aduino", "embedded"]
     }
   }
