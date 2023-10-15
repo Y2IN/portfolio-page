e74e536803a7c113a1ad34a5fceebc984815607f
Y2IN <dpdls0228@daum.net>
2023-10-16 02:07:08 +0900

    메인 문구 수정

--- gatsby-site-meta-data.ts
+++ gatsby-site-meta-data.ts
@@ -1,10 +1,19 @@
 export default {
-    title: `yein's portfolio`,
-    siteUrl: `https://www.yourdomain.tld`,
-    mainBanner: {
-      introduce1: "안녕하세요!",
-      introduce2: "저는",
-      introduce3: "를 좋아하는 개발자 제예인입니다.",
-      skills: ["frontend", "react", "typescript", "python", "c++", "iot", "aduino", "embedded"]
-    }
-  }
+  title: `yein's portfolio`,
+  siteUrl: `https://www.yourdomain.tld`,
+  mainBanner: {
+    introduce1: "안녕하세요!",
+    introduce2: "저는",
+    introduce3: "를 좋아하는 개발자 제예인입니다.",
+    skills: [
+      "frontend",
+      "react",
+      "typescript",
+      "python",
+      "c",
+      "c++",
+      "iot",
+      "embedded",
+    ],
+  },
+};
