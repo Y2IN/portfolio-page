c71135a3ef88401d586ae5e1058edf07466fd86e
Y2IN <dpdls0228@daum.net>
2023-10-16 02:08:22 +0900

    tech stack 틀

--- src/components/techStack/index.styled.ts
+++ src/components/techStack/index.styled.ts
@@ -1,17 +1,62 @@
 import styled from "styled-components";
 
 export const TechStackWrapper = styled.div`
-  width: 100%;
   display: flex;
-  background-color: yellow;
+  width: 100%;
+  height: 100vh;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   height: 100vh;
-  color: #ffffff;
+  color: white;
+  z-index: 1;
 `;
 
-export const TechStackTitle = styled.strong`
-  font-size: 3rem;
-  font-weight: 700;
+export const Text = styled.div`
+  display: flex;
+  text-align: center;
+  font-family: Inter;
+  font-size: 6.25rem;
+  font-style: normal;
+`;
+
+export const skillText = styled.div`
+  display: flex;
+  color: #fbf2f2;
+  text-align: center;
+  font-family: Inter;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 400;
+`;
+
+export const TechStackTitle = styled.div`
+  display: flex;
+  text-align: center;
+  font-family: Inter;
+  font-size: 6.25rem;
+  font-style: normal;
+  line-height: 300%;
+`;
+
+export const StackWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 50%;
+  margin-top: 5%;
+  gap: 3rem;
+`;
+
+export const StackContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 20rem;
+  height: 50rem;
+  border-radius: 1.875rem;
+  border: 1px solid #000000;
+  background: rgba(217, 217, 217, 0.5);
+  opacity: 0.8;
+  padding: 2rem;
+  box-sizing: border-box;
 `;
--- src/components/techStack/index.tsx
+++ src/components/techStack/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import * as S from "./index.styled";
+import { Github_Dark, JavaScript, React_Dark } from "react-skillicons";
 
 interface TechStackProps {
   techStackRef: React.RefObject<HTMLDivElement>;
@@ -10,7 +11,25 @@ const TechStack: React.FC<TechStackProps> = ({
 }: TechStackProps) => {
   return (
     <S.TechStackWrapper id="tech-stack" ref={techStackRef}>
-      <S.TechStackTitle>skill</S.TechStackTitle>
+      <S.TechStackTitle>Once I've Used</S.TechStackTitle>
+      <S.StackWrapper>
+        <S.StackContainer>
+          <S.skillText>LANGUAGES</S.skillText>
+          <React_Dark className="icon" />
+        </S.StackContainer>
+        <S.StackContainer>
+          <S.skillText>FRONTEND</S.skillText>
+        </S.StackContainer>
+        <S.StackContainer>
+          <S.skillText>COMMUNICATION</S.skillText>
+        </S.StackContainer>
+        <S.StackContainer>
+          <S.skillText>DEVOPS</S.skillText>
+        </S.StackContainer>
+        <S.StackContainer>
+          <S.skillText>TOOL</S.skillText>
+        </S.StackContainer>
+      </S.StackWrapper>
     </S.TechStackWrapper>
   );
 };
